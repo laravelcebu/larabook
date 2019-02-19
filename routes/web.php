d19f34539830600f96766914ba169ea7a5a74914
Nino Orendain <ninomasbang@gmail.com>
2019-02-19 11:15:35 -0800

    Laravel Authentication

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/test', function() {
   return 'nino';
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
